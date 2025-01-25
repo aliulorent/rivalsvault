9f3c2771eab01011b09a225cde614dc5cd6f1bd9
Lorent <aliulorent@gmail.com>
2025-01-25 17:46:45 -0500

    Added text & shadow and fixed most alignment

--- components/HeroSelect.vue
+++ components/HeroSelect.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
+import { NuxtImg } from '#components';
+
 const { char } = defineProps(['char']);
 </script>
 <template>
+    <!-- CHARACTER BANNER FOR EACH HERO. MOST PICTURES ARE ABSOLUTE POSITIONING -->
     <div class="relative w-[244px] h-[440px] overflow-hidden p-0 m-4 hover:scale-110">
-        <h3 class="absolute bottom-10 left-2 z-40 text-center font-bold text-white">{{ char.name }}</h3>
-        <svg width="244" height="440" class="z-30 absolute opacity-70" id="charImage">
-            <rect x="0" y="340" width="242" height="100" fill="black"></rect>
-        </svg>
+        <h3 class="absolute bottom-16 left-2 z-40 text-center font-bold text-white">{{ char.name }}</h3>
+        <NuxtImg provider="cloudflare" :src="`/common/bannershadow.webp`" loading="lazy" class="absolute bottom-0 z-30 w-[244px] h-[184px] -mb-[2px]" id="charImage"/>
         <NuxtImg provider="cloudflare" :src="char.image_path" loading="lazy" class="absolute object-cover w-[540px] h-[540px] overflow-clip top-0 left-0 right-0 mx-auto z-20" id="charImage"/>
-        <NuxtImg provider="cloudflare" :src="'/common/img_herolist_herocard_normalbg.webp'" loading="lazy" class="absolute top-0 left-0 z-10 w-[244px] h-[440px]"/>
+        <NuxtImg provider="cloudflare" :src="'/common/staticbg.webp'" loading="lazy" class="absolute top-0 left-0 z-10 w-[244px] h-[440px]" id="charImage"/>
     </div>
 </template>
 <style>
