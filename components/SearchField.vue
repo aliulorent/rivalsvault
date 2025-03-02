2e2a6ba56696ee192a1e8401b88b260d80afdd74
Lorent <aliulorent@gmail.com>
2025-03-02 18:19:09 -0500

    Fixed loading logic

--- components/SearchField.vue
+++ components/SearchField.vue
@@ -16,13 +16,13 @@ const debouncedSearch = useDebounceFn(async ()=>{
                 input: inputField.value
             }
         })
-        isLoading.value = false;
         results.value = data;
+        isLoading.value = false;
     }
     else{
         results.value = [];
     }
-}, 500, { maxWait: 3000});
+}, 1000, { maxWait: 2000});
 
 const handleClick = async (skin: searchQuery) =>{
     console.log("Navigated")
@@ -50,17 +50,16 @@ onBeforeUnmount(() => {
 <template>
     <div class="relative w-[360px] text-black font-[Teko] text-xl">
         <input id="searchBar" type="search" v-model="inputField" @focus="isFocused = true" placeholder="Search Skins" class="px-2 pt-1 rounded-md w-full">
-        <div id="searchResults" v-show="isFocused && results.length > 0" class="absolute w-full top-full left-0 bg-white flex flex-col overflow-y-auto max-h-48} mt-2 rounded-md z-20">
-            <div v-if="isLoading" class="flex-shrink-0 flex items-center">
-                <h3>Loading...</h3>
-            </div>
+        <div v-if="isLoading" class="absolute w-full top-full left-0 bg-white mt-2 rounded-md z-20">
+                <h3 class="px-2">Loading...</h3>
+        </div>
+        <div id="searchResults" v-show="isFocused && results.length > 0" class="absolute w-full top-full left-0 bg-white flex flex-col overflow-y-auto max-h-48 mt-2 rounded-md z-20">
             <div v-for="result in results" class="flex-shrink-0 flex items-center hover:bg-gray-300 cursor-pointer" @click="handleClick(result)">
                 <NuxtImg :src="`${runtime.public.cloudflare}/skins/icon/${result.hero_id}${result.skin_id}.webp`" draggable="false" width="64" height="64" class="w-[24px] h-[24px] sm:w-[32px] sm:h-[32px] lg:w-[36px] lg:h-[36px] z-10" loading="lazy"/>
-                <h3 class="truncate uppercase">{{ result.skin_name }}</h3>
+                <h3 class="truncate uppercase pl-1">{{ result.skin_name }}</h3>
             </div>
         </div>
     </div>
-    
 </template>
 <style>
 
