3ea06b1a9d6534ac0bcac27ad6606b296b134af7
Lorent <aliulorent@gmail.com>
2025-03-03 23:44:06 -0500

    Cleanup comments and unused block

--- pages/[hero]/index.vue
+++ pages/[hero]/index.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
-definePageMeta({
-    // middleware:["slug-check"]
+
+const route = useRoute();
+const slug = route.params.hero;
+const {data} = await useFetch('/api/skins', {
+    method: "get",
+    query:{
+        slug: slug
+    }
 })
-    const route = useRoute();
-    const slug = route.params.hero;
-    const {data} = await useFetch('/api/skins', {
-        method: "get",
-        query:{
-            slug: slug
-        }
-    })
+
 </script>
 <template>
-    <!-- <div v-if="data && data.length>0" class="skins-container h-screen pt-14 flex justify-center items-center"> -->
-        <SkinSelect v-if="data && data.length>0" :skins="data"/>
-        <NotFound v-else/>
-    <!-- </div> -->
+    <SkinSelect v-if="data && data.length>0" :skins="data"/>
+    <NotFound v-else/>
 </template>
