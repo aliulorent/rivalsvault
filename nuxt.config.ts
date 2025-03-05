712669ad32b76aadf69bc6659eeb41392e1d9f3f
Lorent <aliulorent@gmail.com>
2025-03-04 22:40:03 -0500

    Changed rendering to SWR for all pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
+  routeRules:{
+    '/**': { swr: 3600 }
+  },
   app:{
     head:{
       titleTemplate: '%s %separator %siteName',
