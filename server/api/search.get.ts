45c910a4016278b430a018b08fa6644dba02ee64
Lorent <aliulorent@gmail.com>
2025-03-04 22:30:53 -0500

    Made both DB calls concurrently instead of sync

--- server/api/search.get.ts
+++ server/api/search.get.ts
@@ -1,18 +1,21 @@
 export default defineEventHandler(async (event)=>{
     const DB = event.context.cloudflare.env.DB;
     const query = getQuery(event);
-    const dbQuery = await DB.prepare(
-        `SELECT s.skin_id, c.hero_id, s.skin_name, c.hero_name, c.slug FROM skins s INNER JOIN characters c ON s.hero_id = c.hero_id
-         WHERE skin_name LIKE ?`,
-    )
-    .bind(`%${query.input}%`)
-    .all<searchQuery>();
-    const dbQuery2 = await DB.prepare(
-        `SELECT * FROM characters
-         WHERE hero_name LIKE ?`,
-    )
-    .bind(`%${query.input}%`)
-    .all<charactersQuery>();
+    const [dbQuery, dbQuery2] = await Promise.all([
+        DB.prepare(
+            `SELECT s.skin_id, c.hero_id, s.skin_name, c.hero_name, c.slug FROM skins s INNER JOIN characters c ON s.hero_id = c.hero_id
+             WHERE skin_name LIKE ?`,
+        )
+        .bind(`%${query.input}%`)
+        .all<searchQuery>(),
+        
+        DB.prepare(
+            `SELECT * FROM characters
+             WHERE hero_name LIKE ?`,
+        )
+        .bind(`%${query.input}%`)
+        .all<charactersQuery>()
+    ])
     return {
         skins: dbQuery.results,
         heroes: dbQuery2.results
