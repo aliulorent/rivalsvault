04597af96b66216057e3eda53b2d641dfb910004
Lorent <aliulorent@gmail.com>
2025-03-06 02:26:31 -0500

    Switched site logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
-const clickHandler = () =>{
-    return navigateTo("/");
+const runtime = useRuntimeConfig();
+const clickHandler = async () =>{
+    return await navigateTo("/");
 }
 </script>
 <template>
-    <div id="navbar" class="w-full h-14 bg-background-900 flex justify-between items-center text-text-50 p-4 border-b-2 border-b-background-950 fixed top-0 z-20">
-    <div @click="clickHandler" class="flex gap-2 cursor-pointer">
-        <div class="w-[64px]">
-            <NuxtImg provider="cloudflare" :src="`/common/rivalslogo.webp`"/>
-        </div>
-        <h2 class="font-[Teko] font-[400] text-2xl">VAULT</h2>
+    <div id="navbar" class="w-full h-14 bg-background-900 flex justify-between items-center text-text-50 p-4 border-b-2 border-b-background-900 fixed top-0 z-20">
+    <div @click="clickHandler" class="flex items-center gap-1 cursor-pointer">
+        <NuxtImg :src="`${runtime.public.cloudflare}/common/rivalsvaultlogosmall.webp`" width="256" height="256" class="w-[48px] h-[48px]"/>
+        <h2 class="font-[Teko] font-[400] text-3xl">Rivals <span class="text-secondary-500">Vault</span></h2>
     </div>
     <div>
         <SearchField/>
@@ -20,10 +19,10 @@ const clickHandler = () =>{
         <div id="footer" class="h-12 p-4 flex justify-between items-center gap-4 bg-background-900 text-text-50 text-sm box-border">
         <p class="truncate">© 2025 NetEase Games. All rights reserved. Marvel Rivals and all related assets are the property of NetEase Games and/or their respective owners. Rivals Vault is not affiliated with NetEase Games or Marvel.</p>
         <div class="flex items-center gap-4">
-            <NuxtLink to="/privacy" class="justify-self-end">
+            <NuxtLink to="/privacy-policy" class="justify-self-end">
                 <p>Privacy Policy</p>
             </NuxtLink>
-            <NuxtLink to="/cookies" class="justify-self-end">
+            <NuxtLink to="/cookie-policy" class="justify-self-end">
                 <p>Cookies Policy</p>
             </NuxtLink>
         </div>
